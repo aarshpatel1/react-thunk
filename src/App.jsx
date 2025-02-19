7df4ebfef0ec612347eb870b87d2cff652139660
aarshpatel1 <aarsh18patel@gmail.com>
2025-02-19 18:30:30 +0530

    remove unnecessary class

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
 	return (
 		<Provider store={store}>
-			<div className="app">
-				<TodoList />
-			</div>
+			<TodoList />
 		</Provider>
 	);
 }
